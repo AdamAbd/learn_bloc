e78a0248a6188ca3cf8b3aa945de7e03e7cc2078
AdamAbd <adamabd14113@gmail.com>
2022-03-18 14:57:43 +0700

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -45,96 +45,104 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<InternetCubit, InternetState>(
-      listener: (context, state) {
-        if (state is InternetConnected &&
-            state.connectionType == ConnectionType.wifi) {
-          return context.read<CounterCubit>().increment();
-        } else if (state is InternetConnected &&
-            state.connectionType == ConnectionType.mobile) {
-          return context.read<CounterCubit>().increment();
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              BlocBuilder<InternetCubit, InternetState>(
-                builder: (context, state) {
-                  if (state is InternetConnected &&
-                      state.connectionType == ConnectionType.wifi) {
-                    return Text('Wifi');
-                  } else if (state is InternetConnected &&
-                      state.connectionType == ConnectionType.mobile) {
-                    return Text('Mobile');
-                  } else {
-                    return Text('Disconnect');
-                  }
-                },
-              ),
-              BlocConsumer<CounterCubit, CounterState>(
-                listener: (context, state) {
-                  if (state.wasIncremented == true) {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text('Incremendted!'),
-                      duration: Duration(milliseconds: 100),
-                    ));
-                  } else if (state.wasIncremented == false) {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text('Decremented!'),
-                      duration: Duration(milliseconds: 100),
-                    ));
-                  }
-                },
-                builder: (context, state) {
-                  if (state.counterValue < 0) {
-                    return Text(
-                      'BRR, Negative ${state.counterValue}',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else if (state.counterValue % 2 == 0) {
-                    return Text(
-                      'YaaaY ${state.counterValue}',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else if (state.counterValue == 5) {
-                    return Text(
-                      'Hmmm, Number ${state.counterValue}',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  } else {
-                    return Text(
-                      '${state.counterValue}',
-                      style: Theme.of(context).textTheme.headline4,
-                    );
-                  }
-                },
-              ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //   children: [
-              //     FloatingActionButton(
-              //       onPressed: () {
-              //         context.read<CounterCubit>().decrement();
-              //       },
-              //       tooltip: 'Increment',
-              //       child: const Icon(Icons.remove),
-              //     ),
-              //     FloatingActionButton(
-              //       onPressed: () {
-              //         context.read<CounterCubit>().increment();
-              //       },
-              //       tooltip: 'Increment',
-              //       child: const Icon(Icons.add),
-              //     ),
-              //   ],
-              // ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<InternetCubit, InternetState>(
+              builder: (context, state) {
+                if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.wifi) {
+                  return Text('Wifi');
+                } else if (state is InternetConnected &&
+                    state.connectionType == ConnectionType.mobile) {
+                  return Text('Mobile');
+                } else {
+                  return Text('Disconnect');
+                }
+              },
+            ),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented == true) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('Incremendted!'),
+                    duration: Duration(milliseconds: 100),
+                  ));
+                } else if (state.wasIncremented == false) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('Decremented!'),
+                    duration: Duration(milliseconds: 100),
+                  ));
+                }
+              },
+              builder: (context, state) {
+                if (state.counterValue < 0) {
+                  return Text(
+                    'BRR, Negative ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue % 2 == 0) {
+                  return Text(
+                    'YaaaY ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else if (state.counterValue == 5) {
+                  return Text(
+                    'Hmmm, Number ${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                } else {
+                  return Text(
+                    '${state.counterValue}',
+                    style: Theme.of(context).textTheme.headline4,
+                  );
+                }
+              },
+            ),
+            Builder(
+              builder: (context) {
+                final internetState = context.watch<InternetCubit>().state;
+                final counterState = context.watch<CounterCubit>().state;
+
+                if (internetState is InternetConnected &&
+                    internetState.connectionType == ConnectionType.wifi) {
+                  return Text(
+                    'Counter: ${counterState.counterValue} & Internet: Wifi',
+                  );
+                } else if (internetState is InternetConnected &&
+                    internetState.connectionType == ConnectionType.mobile) {
+                  return Text(
+                    'Counter: ${counterState.counterValue} & Internet: Mobile',
+                  );
+                }
+                return Text('Counter: ${counterState.counterValue}');
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    context.read<CounterCubit>().decrement();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    context.read<CounterCubit>().increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
